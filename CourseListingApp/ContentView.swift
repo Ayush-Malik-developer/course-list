90249a26ca181b3414ee6dc816120cb9f31e4249
Ayush Malik <ayushmalik461999@gmail.com>
2026-02-24 02:41:36 +0530

    initial commit

---
+++ CourseListingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CourseListingApp
+//
+//  Created by Ayush Malik on 24/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CourseListingApp/CourseListingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseListingAppApp.swift
+//  CourseListingApp
+//
+//  Created by Ayush Malik on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CourseListingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
